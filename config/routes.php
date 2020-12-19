9296c1ea2090694ea158877457fe1d1f0edcd047
Denis Feliciano <denisf.salles@gmail.com>
2020-12-19 16:02:34 -0300

    add new routes

--- config/routes.php
+++ config/routes.php
@@ -5,11 +5,14 @@ use Denis\MVC\Controllers\CursosControllers;
 $routes = [
     'GET' => [
         '/cursos' => [CursosControllers::class, 'index'],
-        '/cursos/novo' => [CursosControllers::class, 'create']
+        '/cursos/novo' => [CursosControllers::class, 'create'],
+        '/cursos/delete' => [CursosControllers::class, 'destroy']
     ],
     'POST' =>  [
-        '/cursos/novo' => [CursosControllers::class, 'store']
-    ],
+    '/cursos/novo' => [CursosControllers::class, 'store'],
+],
+    'DELETE' => [
+]
    
 ];
 return $routes;
